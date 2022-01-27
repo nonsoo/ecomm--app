dc2441cbd843749b05ee50dc8c4c3fd01dd0a4a5
Nonso <nonsootoh@gmail.com>
2022-01-27 13:03:07 -0500

    final touches

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { IoTrashOutline } from "react-icons/io5";
 
+import { useDispatch } from "react-redux";
+import { removeItem } from "../redux/Shopingcart";
+
 const CartItem = ({ imgName, itemName, itemPrice, itemQty, itemSubTotal }) => {
+  const dispatch = useDispatch();
   return (
     <div className="cartItem">
       <img src={imgName} alt="thumnail" className="cartItem__img" />
@@ -11,7 +15,10 @@ const CartItem = ({ imgName, itemName, itemPrice, itemQty, itemSubTotal }) => {
           {`$${itemPrice}`} x {itemQty} <span>$ {`$${itemSubTotal}`}</span>
         </p>
       </div>
-      <IoTrashOutline className="cartItem__Trash" />
+      <IoTrashOutline
+        className="cartItem__Trash"
+        onClick={() => dispatch(removeItem())}
+      />
     </div>
   );
 };
