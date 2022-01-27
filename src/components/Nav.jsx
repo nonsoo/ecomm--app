27964ec87e8ea0d147508b73293ce91c819e00f2
Nonso <nonsootoh@gmail.com>
2022-01-27 11:06:02 -0500

    conditional render

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdShoppingCart } from "react-icons/md";
 import ImageAvatar from "../images/image-avatar.png";
 
 import ShoppingCart from "./ShopCart";
 
 const Nav = () => {
+  const [showCart, setShowCart] = useState(false);
   return (
     <>
       <nav className="nav">
@@ -17,11 +18,14 @@ const Nav = () => {
           <p className="nav__NavItem">Contact</p>
         </div>
         <div className="nav__RightCon">
-          <MdShoppingCart className="nav__Cart" />
+          <MdShoppingCart
+            className="nav__Cart"
+            onClick={() => setShowCart((state) => !state)}
+          />
           <img src={ImageAvatar} alt="logged in user" className="nav__Image" />
         </div>
       </nav>
-      <ShoppingCart />
+      {showCart && <ShoppingCart />}
     </>
   );
 };
